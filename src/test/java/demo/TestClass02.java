8970c41140d6bbeb5fe75601c9667b55ef7fa4a9
Albab97 <albabahmed21@yahoo.com>
2025-04-14 12:05:00 +0530

    Priority and Enabled

---
+++ src/test/java/demo/TestClass02.java
@@ -0,0 +1,21 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class TestClass02 {
+    // All test methods are by default executed in alphabetical order.
+    //Priority allows us to decide the order of execution. If the priority of two or more methods are same then it will be executed in Alphabetical order.
+    @Test(priority = 0)
+    public void registration(){
+        System.out.println("In the registration test method");
+    }
+    // enable will allow us to ignore/disable the execution of a particular test method. By default enable is true for all the methods.
+    @Test(priority = 2, enabled = false)
+    public void logout(){
+        System.out.println("In the logout test method");
+    }
+    @Test(priority = 1)
+    public void login(){
+        System.out.println("In the login test method");
+    }
+}
